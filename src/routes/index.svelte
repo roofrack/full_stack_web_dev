6e7a8fbc556de9c052f8c6574e8a48cb76082f6f
roofrack <robaylard@gmail.com>
2022-04-18 22:22:54 -0700

    Develop html structure

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,22 @@
-<h1>Todos</h1>
+<script>
+    import TodoItem from "$lib/todo-item.svelte"
+
+    const title = "Todo";
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+
+<h1>{title}</h1>
 
 <form action="" method="">
     <input type="text" name="text" aria-label="Add a todo" placeholder="+ type to add a todo"/>
 </form>
+
+<br>
+<TodoItem />
+<br>
+<TodoItem />
+<br>
+<TodoItem />
